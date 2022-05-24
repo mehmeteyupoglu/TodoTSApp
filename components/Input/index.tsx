c2c6d671a3112d09874985cde5e2027a0273d24e
mehmeteyupoglu-osf <mehmeteyupoglu2@gmail.com>
2022-05-24 14:43:11 +0300

    feat: add dynamic color for the checkbox

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Text, View, TextInput, StyleSheet } from 'react-native';
 import Icon from 'react-native-ionicons';
 
@@ -11,34 +11,60 @@ interface Props {
 }
 
 const Input: FC<Props> = ({ icon, placeholder, onChangeText }) => {
+
+    const [checked, setChecked] = useState<boolean>(false);
+    const handlePress = () => {
+        // set color and icon for checkbox
+        setChecked(true);
+        setTimeout(() => {
+            setChecked(false);
+        }, 1000)
+    }
+
     return (
-        <View style={styles.container}>
-            <View style={styles.input}>
-                <Icon name={icon} size={22} color="#555" style={styles.icon}/>
-                <TextInput placeholderTextColor='#555' placeholder={placeholder} onChangeText={onChangeText} />
-                <Icon name="md-checkbox-outline" size={27.5} color="black" style={{marginRight: 10, marginLeft: 'auto' }}/>
+        <View style={styles().container}>
+            <View style={styles().input}>
+                <Icon name={icon} size={22} color="#555" style={styles().icon} />
+                <TextInput
+                    placeholderTextColor='#555'
+                    placeholder={placeholder}
+                    onChangeText={onChangeText}
+                />
+                <Icon
+                    name={checked ? "md-checkbox" : "md-checkbox-outline"}
+                    size={27.5}
+                    color="black"
+                    style={ styles(checked).checkboxIcon }
+                    onPress={handlePress}
+                />
             </View>
         </View>
     );
 };
 
-const styles = StyleSheet.create({
+const styles = (checked = false) => StyleSheet.create({
     container: {
         flexDirection: 'row',
         alignItems: 'center',
+    },
+
+    input: {
+        margin: 20,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 5,
+        backgroundColor: '#fff',
     },
 
     icon: {
         padding: 5,
     },
 
-    input: {
-        margin: 20, 
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 5,
-        backgroundColor: '#fff',
+    checkboxIcon: {
+        marginRight: 10,
+        marginLeft: 'auto', 
+        color: checked ? 'green' : 'black'
     }
 });
 
