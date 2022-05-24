253520914fe660441bd2f9b1a129b770a75be14e
mehmeteyupoglu-osf <mehmeteyupoglu2@gmail.com>
2022-05-24 12:14:15 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+// import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+        <Text>Hello world!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+export default App;
